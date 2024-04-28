d48bee8b622023a40a5d38215f913d0a44aad1e7
SilvieFlidrova <flidrova@spsostrov.cz>
2024-04-28 15:46:49 +0200

    upravené indexy, aby vše mohlo být v jednom kódu

--- BOR/Lekce3_1/Program.cs
+++ BOR/Lekce3_1/Program.cs
@@ -7,101 +7,96 @@ namespace Lekce3_1
 
     internal class Program
     {
-        /*   //Ukol - opravte v tomto textu omylem zapnuty Caps Lock
-           //string capsLock = "jAK mICROSOFT wORD POZNA ZAPNUTY cAPSLOCK";
+        //Ukol - opravte v tomto textu omylem zapnuty Caps Lock
+        //string capsLock = "jAK mICROSOFT wORD POZNA ZAPNUTY cAPSLOCK";
 
-           static void Main(string[] args)
-           {
-               string capsLock = "jAK mICROSOFT wORD POZNA ZAPNUTY cAPSLOCK";
-
-               //převedu string na pole znaků
-
-               char[] pismeno = capsLock.ToCharArray();
-
-
-               //kontrola každého znaku z řetězce
-
-               for (int i = 0; i < pismeno.Length; i++)
-               {
-                   //pokud je znak písmeno, převádím na malé/velké, pokud není, nechám beze změny
-
-                   if (char.IsLetter(pismeno[i]))
-                   {
-                       if (char.IsLower(pismeno[i]))
-                       {
-                           pismeno[i] = char.ToUpper(pismeno[i]);
-                       }
-                       else
-                       {
-                           pismeno[i] = char.ToLower(pismeno[i]);
-                       }
-                   }
-               }
-
-               //vrátím nový string sestavený z převedených písmen
-
-               string opravenyText = new string(pismeno);
-               Console.WriteLine(opravenyText);
-        
-
-        //Ukol - rozsifrujte tuto zpravu - capsLock byl zasifrovan tak, ze jsme kazde pismeno posunuli o jedno doprava: 'a' -> 'b'. 
-        //string sifra = "Wzcpsob!qsbdf!.!hsbuvmvkj!b!ktfn!ob!Ufcf!qztoz";
-        static void Main()
+        static void Main(string[] args)
         {
+            string capsLock = "jAK mICROSOFT wORD POZNA ZAPNUTY cAPSLOCK";
+
+            //převedu string na pole znaků
+
+            char[] pismeno = capsLock.ToCharArray();
+
+
+            //kontrola každého znaku z řetězce
+
+            for (int i = 0; i < pismeno.Length; i++)
+            {
+                //pokud je znak písmeno, převádím na malé/velké, pokud není, nechám beze změny
+
+                if (char.IsLetter(pismeno[i]))
+                {
+                    if (char.IsLower(pismeno[i]))
+                    {
+                        pismeno[i] = char.ToUpper(pismeno[i]);
+                    }
+                    else
+                    {
+                        pismeno[i] = char.ToLower(pismeno[i]);
+                    }
+                }
+            }
+
+            //vrátím nový string sestavený z převedených písmen
+
+            string opravenyText = new string(pismeno);
+            Console.WriteLine(opravenyText);
+
+
+            //Ukol - rozsifrujte tuto zpravu - capsLock byl zasifrovan tak, ze jsme kazde pismeno posunuli o jedno doprava: 'a' -> 'b'. 
+            //string sifra = "Wzcpsob!qsbdf!.!hsbuvmvkj!b!ktfn!ob!Ufcf!qztoz";
+
             string zasifrovanyText = "Wzcpsob!qsbdf!.!hsbuvmvkj!b!ktfn!ob!Ufcf!qztoz";
             char[] poleZnaku = zasifrovanyText.ToCharArray();
 
-            for (int i = 0; i < zasifrovanyText.Length; i++)
+            for (int j = 0; j < zasifrovanyText.Length; j++)
             {
                 //pro každý znak pole provedu posunutí o jedno doleva, tj. musím ho zmenšit o 1
-                
-                char znak = poleZnaku[i];
+
+                char znak = poleZnaku[j];
                 char posunutyZnak = (char)(znak - 1);
-                poleZnaku[i] = posunutyZnak;
+                poleZnaku[j] = posunutyZnak;
             }
 
             string desifrovanyText = new string(poleZnaku);
             Console.WriteLine(desifrovanyText);
-        }
-       
 
-        //Ukol - Obratte poradi stringu
-        //string palindrom = "Kuna nese nanuk"; 
-        static void Main()
-        {
+
+
+            //Ukol - Obratte poradi stringu
+            //string palindrom = "Kuna nese nanuk"; 
+
             string palindrom = "Kuna nese nanuk";
 
-            char[] poleZnaku = palindrom.ToCharArray();
+            char[] polePismen = palindrom.ToCharArray();
             // Převedení stringu na pole znaků
 
-            // Ukazatele pro první(i) a poslední(j) znak v poli
-            int i = 0;
-            int j = poleZnaku.Length - 1;
+            // Ukazatele pro první(a) a poslední(b) znak v poli
+            int a = 0;
+            int b = polePismen.Length - 1;
 
             // Prohození znaků 
-            while (i < j)
+            while (a < b)
             {
-                char mezipamet = poleZnaku[i];
-                poleZnaku[i] = poleZnaku[j];
-                poleZnaku[j] = mezipamet;
+                char mezipamet = polePismen[a];
+                polePismen[a] = polePismen[b];
+                polePismen[b] = mezipamet;
 
-                i++;
-                j--;
+                a++;
+                b--;
             }
 
             // Vytvoření nového stringu z obráceného pole znaků
-            string obracenyPalindrom = new string(poleZnaku);
+            string obracenyPalindrom = new string(polePismen);
 
             Console.WriteLine($"Obrácený palindrom:{obracenyPalindrom}");
-        }
-        */
 
 
-        //Ukol - Napiste funkci, ktera umi detekovat, ze se jedna o palindrom (zatim jen u slov) a pak z pole vypiste jen palindromy
-        //string[] poleSlov = new string[] { "kajak", "program", "rotor", "Czechitas", "madam", "Jarda", "radar", "nepotopen" }; 
+            //Ukol - Napiste funkci, ktera umi detekovat, ze se jedna o palindrom (zatim jen u slov) a pak z pole vypiste jen palindromy
+            //string[] poleSlov = new string[] { "kajak", "program", "rotor", "Czechitas", "madam", "Jarda", "radar", "nepotopen" }; 
 
-        static void Main()
-        {
+
 
             string[] poleSlov = new string[] { "kajak", "program", "rotor", "Czechitas", "madam", "Jarda", "radar", "nepotopen" };
 
@@ -111,9 +106,9 @@ namespace Lekce3_1
 
             Console.WriteLine($"Palindromy z daných slov jsou:");
 
-            for (int i = 0; i < poleSlov.Length; i++)
+            for (int k = 0; k < poleSlov.Length; k++)
             {
-                string aktualniSlovo = poleSlov[i];
+                string aktualniSlovo = poleSlov[k];
                 bool JePalindrom = true;
 
                 int poradiPismenkaZacatekSlova = 0;
@@ -133,14 +128,15 @@ namespace Lekce3_1
                 {
                     Console.WriteLine(aktualniSlovo);
                 }
-
-
             }
 
         }
+
     }
+
 }
-      
+
+
 
 
 
