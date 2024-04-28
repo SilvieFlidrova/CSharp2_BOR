aace7d4164d99f8ed3b54789f69f32160a41623c
SilvieFlidrova <flidrova@spsostrov.cz>
2024-04-28 14:11:59 +0200

    sifra

--- BOR/Lekce3_1/Program.cs
+++ BOR/Lekce3_1/Program.cs
@@ -6,47 +6,69 @@
     //Ukol - Napiste funkci, ktera umi detekovat, ze se jedna o palindrom (zatim jen u slov) a pak z pole vypiste jen palindromy
     //string[] slova = new string[] { "kajak", "program", "rotor", "Czechitas", "madam", "Jarda", "radar", "nepotopen" }; 
 
-    //Ukol - rozsifrujte tuto zpravu - capsLock byl zasifrovan tak, ze jsme kazde pismeno posunuli o jedno doprava: 'a' -> 'b'. 
-    //string sifra = "Wzcpsob!qsbdf!.!hsbuvmvkj!b!ktfn!ob!Ufcf!qztoz";
-
     internal class Program
     {
-        //Ukol - opravte v tomto textu omylem zapnuty Caps Lock
-        //string capsLock = "jAK mICROSOFT wORD POZNA ZAPNUTY cAPSLOCK";
+        /*   //Ukol - opravte v tomto textu omylem zapnuty Caps Lock
+           //string capsLock = "jAK mICROSOFT wORD POZNA ZAPNUTY cAPSLOCK";
 
-        static void Main(string[] args)
+           static void Main(string[] args)
+           {
+               string capsLock = "jAK mICROSOFT wORD POZNA ZAPNUTY cAPSLOCK";
+
+               //převedu string na pole znaků
+
+               char[] pismeno = capsLock.ToCharArray();
+
+
+               //kontrola každého znaku z řetězce
+
+               for (int i = 0; i < pismeno.Length; i++)
+               {
+                   //pokud je znak písmeno, převádím na malé/velké, pokud není, nechám beze změny
+
+                   if (char.IsLetter(pismeno[i]))
+                   {
+                       if (char.IsLower(pismeno[i]))
+                       {
+                           pismeno[i] = char.ToUpper(pismeno[i]);
+                       }
+                       else
+                       {
+                           pismeno[i] = char.ToLower(pismeno[i]);
+                       }
+                   }
+               }
+
+               //vrátím nový string sestavený z převedených písmen
+
+               string opravenyText = new string(pismeno);
+               Console.WriteLine(opravenyText);
+        */
+
+        //Ukol - rozsifrujte tuto zpravu - capsLock byl zasifrovan tak, ze jsme kazde pismeno posunuli o jedno doprava: 'a' -> 'b'. 
+        //string sifra = "Wzcpsob!qsbdf!.!hsbuvmvkj!b!ktfn!ob!Ufcf!qztoz";
+        static void Main()
         {
-            string capsLock = "jAK mICROSOFT wORD POZNA ZAPNUTY cAPSLOCK";
+            string zasifrovanyText = "Wzcpsob!qsbdf!.!hsbuvmvkj!b!ktfn!ob!Ufcf!qztoz";
+            char[] znaky = zasifrovanyText.ToCharArray();
 
-            //převedu string na pole znaků
-
-            char[] pismeno = capsLock.ToCharArray();
-
-
-            //kontrola každého znaku z řetězce
-
-            for (int i = 0; i < pismeno.Length; i++)
+            for (int i = 0; i < zasifrovanyText.Length; i++)
             {
-                //pokud je znak písmeno, převádím na malé/velké, pokud není, nechám beze změny
-
-                if (char.IsLetter(pismeno[i]))
-                {
-                    if (char.IsLower(pismeno[i]))
-                    {
-                        pismeno[i] = char.ToUpper(pismeno[i]);
-                    }
-                    else
-                    {
-                        pismeno[i] = char.ToLower(pismeno[i]);
-                    }
-                }
+                //pro každý znak pole provedu posunutí o jedno doleva, tj. musím ho zmenšit o 1
+                
+                char znak = znaky[i];
+                char posunutyZnak = (char)(znak - 1);
+                znaky[i] = posunutyZnak;
             }
 
-            //vrátím nový string sestavený z převedených písmen
+            string desifrovanyText = new string(znaky);
+            Console.WriteLine(desifrovanyText);
+        }
 
-            string opravenyText = new string(pismeno);
-            Console.WriteLine(opravenyText);
-
+            
         }
     }
-}
+
+    
+
+
